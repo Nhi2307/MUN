d4b2ed3e2aa3541bbc4747844e5196218d854cfb
ngochua9893 <144428368+ngochua9893@users.noreply.github.com>
2024-03-23 12:56:55 +0700

    Add files via upload

---
+++ bt5.1.1.cpp
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//#include<conio.h>
+////input array
+//void NhapMang(int a[], int &n)
+//{
+//	printf("\n nhap so phan tu cua mang:");
+//	scanf_s("%d", &n);
+//	for (int i = 0; i<n; i++)
+//	{
+//		printf("nhap a[%d]= ", i); scanf_s("%d", &a[i]);
+//	}
+//}
+//void XuatMang(int a[], int n)
+//{
+//	printf("Noi dung mang:");
+//	for (int i = 0; i<n; i++)
+//		printf("%3d", a[i]);
+//}
+//
+//int main(){
+//	int a[100], n;
+//	NhapMang(a, n);
+//	XuatMang(a, n);
+//	_getch();
+//}
